efbdd40b57bcaee184b2509b308c694ef16ded48
Gustavo <guvono4@gmail.com>
2024-04-24 21:40:46 -0300

    update: reaproveitamento de codigo utilizando funcoes e novas interacoes com o usuario

--- matriz/Program.cs
+++ matriz/Program.cs
@@ -1,90 +1,127 @@
-﻿int qtd_linhas = 5, qtd_colunas = 5;
-int[,] matriz1 = new int[qtd_linhas, qtd_colunas];
-int[,] matriz2 = new int[qtd_linhas, qtd_colunas];
-int[,] matriz3 = new int[qtd_linhas, qtd_colunas];
+﻿int qtd_linha = 0, qtd_coluna = 0, menorNumero = 0, maiorNumero = 0, sinal = 0, erroSinal = 0, calculoPronto = 0, restart = 0;
+int[,] matriz1, matriz2, matriz3;
 
-//CRIANDO A MATRIZ ORIGINAL
-Console.WriteLine("Matriz ORIGINAL 1:");
-for (int l = 0; l < qtd_linhas; l++)
-{
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
-    {
-        matriz1[l, c] = new Random().Next(1, 10);
-        Console.Write(matriz1[l, c] + " ");
-    }
-}
-Console.WriteLine();
 
-Console.WriteLine("Matriz ORIGINAL 2:");
-for (int l = 0; l < qtd_linhas; l++)
+//FUNCOES
+//ESTA FUNCAO CRIA AS MATRIZES E AS IMPRIME NA TELA
+void criarMatrizes(int linhas, int colunas, int menorN, int maiorN)
 {
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
-    {
-        matriz2[l, c] = new Random().Next(1, 10);
-        Console.Write(matriz2[l, c] + " ");
-    }
-}
-Console.WriteLine();
+    matriz1 = new int[linhas, colunas];
+    matriz2 = new int[linhas, colunas];
+    matriz3 = new int[linhas, colunas];
 
-//OPERAÇÕES
-Console.WriteLine("\nSOMA:");
-for (int l = 0;l < qtd_colunas; l++)
-{
-    if (l > 0) Console.WriteLine();
-    for (int c = 0;c < qtd_colunas; c++)
+    Console.WriteLine("MATRIZ 1:");
+    for (int l = 0; l < linhas; l++)
     {
-        matriz3[l, c] = matriz1[l, c] + matriz2[l, c];
-        Console.Write(matriz3[l, c] + " ");
-    }
-}
-Console.WriteLine();
+        if (l > 0) Console.WriteLine();
+        for (int c = 0; c < colunas; c++)
+        {
+            matriz1[l, c] = new Random().Next(menorN, maiorN);
+            Console.Write(matriz1[l, c] + " ");
+        }
+    };
 
-Console.WriteLine("\nSUBTRAÇÃO:");
-for (int l = 0; l < qtd_colunas; l++)
-{
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
-    {
-        matriz3[l, c] = matriz1[l, c] - matriz2[l, c];
-        Console.Write(matriz3[l, c] + " ");
-    }
-}
-Console.WriteLine();
+    Console.WriteLine();
 
-Console.WriteLine("\nMULTIPLICAÇÃO:");
-for (int l = 0; l < qtd_colunas; l++)
-{
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
+    Console.WriteLine("MATRIZ 2");
+    for (int l = 0; l < linhas; l++)
     {
-        matriz3[l, c] = matriz1[l, c] * matriz2[l, c];
-        Console.Write(matriz3[l, c] + " ");
-    }
+        if (l > 0) Console.WriteLine();
+        for (int c = 0; c < colunas; c++)
+        {
+            matriz2[l, c] = new Random().Next(menorN, maiorN);
+            Console.Write(matriz2[l, c] + " ");
+        }
+    };
 }
-Console.WriteLine();
 
-Console.WriteLine("\nDIVISÃO:");
-for (int l = 0; l < qtd_colunas; l++)
+void calcularMatriz(string mensagem, int sinal, int[,] primeiraMatriz, int[,] segundaMatriz)
 {
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
-    {
-        matriz3[l, c] = matriz1[l, c] / matriz2[l, c];
-        Console.Write(matriz3[l, c] + " ");
-    }
-}
-Console.WriteLine();
+    if (calculoPronto == 1) return;
 
-Console.WriteLine("\nRESTOS DA MATRIZ DE DIVISÃO:");
-for (int l = 0; l < qtd_colunas; l++)
-{
-    if (l > 0) Console.WriteLine();
-    for (int c = 0; c < qtd_colunas; c++)
+    for (int l = 0; l < qtd_linha; l++)
     {
-        matriz3[l, c] = matriz1[l, c] % matriz2[l, c];
-        Console.Write(matriz3[l, c] + " ");
+        if (l > 0) Console.WriteLine();
+        for (int c = 0; c < qtd_coluna; c++)
+        {
+            switch (sinal)
+            {
+                case 1:
+                    matriz3[l, c] = primeiraMatriz[l, c] + segundaMatriz[l, c];
+                    Console.Write(matriz3[l, c] + " ");
+                    break;
+                case 2:
+                    matriz3[l, c] = primeiraMatriz[l, c] - segundaMatriz[l, c];
+                    Console.Write(matriz3[l, c] + " ");
+                    break;
+                case 3:
+                    matriz3[l, c] = primeiraMatriz[l, c] * segundaMatriz[l, c];
+                    Console.Write(matriz3[l, c] + " ");
+                    break;
+                case 4:
+                    matriz3[l, c] = primeiraMatriz[l, c] / segundaMatriz[l, c];
+                    Console.Write(matriz3[l, c] + " ");
+                    break;
+                case 5:
+                    matriz3[l, c] = primeiraMatriz[l, c] % segundaMatriz[l, c];
+                    Console.Write(matriz3[l, c] + " ");
+                    break;
+            }
+        }
     }
+    calculoPronto = 1;
 }
-Console.WriteLine();
+
+//INICIO DA INTERACAO COM O USUARIO
+do
+{
+    Console.Clear();
+    //CRIANDO AS MATRIZES ORIGINAIS
+    Console.Write("\nInforme a quantidade de LINHAS das matrizes: ");
+    qtd_linha = int.Parse(Console.ReadLine());
+
+    Console.Write("\nInforme a quantidade de COLUNAS das matrizes: ");
+    qtd_coluna = int.Parse(Console.ReadLine());
+
+    Console.Write("\nInforme qual será o MENOR número que terá nas matrizes: ");
+    menorNumero = int.Parse(Console.ReadLine());
+
+    Console.Write("\nInforme qual será o MAIOR número que terá nas matrizes: ");
+    maiorNumero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Matrizes ORIGINAIS:");
+    criarMatrizes(qtd_linha, qtd_coluna, menorNumero, maiorNumero);
+    Console.WriteLine();
+
+    do
+    {
+        if (erroSinal == 1) Console.WriteLine("\nERRO: Favor informar o sinal corretamente:");
+
+        Console.WriteLine("Qual operação deseja realizar?");
+        Console.WriteLine("1 - SOMAR;");
+        Console.WriteLine("2 - SUBTRAIR;");
+        Console.WriteLine("3 - MULTIPLICAR;");
+        Console.WriteLine("4 - DIVIDIR;");
+        Console.WriteLine("5 - RESTO;");
+        sinal = int.Parse(Console.ReadLine());
+    } while (sinal < 1 || sinal > 5);
+    erroSinal = 0;
+
+    Console.WriteLine();
+
+    //OPERAÇÕES
+    calcularMatriz("\n\nSOMA: ", sinal, matriz1, matriz2);
+
+    calcularMatriz("\n\nSUBTRAÇÃO: ", sinal, matriz1, matriz2);
+
+    calcularMatriz("\n\nMULTIPLICAÇÃO: ", sinal, matriz1, matriz2);
+
+    calcularMatriz("\n\nDIVISÃO: ", sinal, matriz1, matriz2);
+
+    calcularMatriz("\n\nRESTOS DA DIVISÃO: ", sinal, matriz1, matriz2);
+
+    Console.WriteLine("\n\nReiniciar programa?");
+    Console.WriteLine("1 - SIM");
+    Console.WriteLine("0 - NÃO");
+    restart = int.Parse(Console.ReadLine());
+} while (restart == 1);
